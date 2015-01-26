394051d78851b6ec50e75a50c74238b99b8b62f0
bpaine1 <bpaine@ucsc.edu>
2015-01-25 22:51:53 -0800

    network should work

---
+++ KrabKlashers/Assets/camdeactivator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camdeactivator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// deactivate the CharacterController that is not Mine in the Network 
+		if (!networkView.isMine) {
+			GetComponent<CharacterController>().enabled = false;
+			GetComponentInChildren<AudioListener>().enabled = false;
+			GetComponentInChildren<Camera>().enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
