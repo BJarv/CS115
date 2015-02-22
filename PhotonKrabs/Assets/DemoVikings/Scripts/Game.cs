a14f676310a53d93f229ffd3f2161e75a94f1fc4
BJarv <brandon.jarv@gmail.com>
2015-02-21 20:18:47 -0800

    ATTACK PROTOTYPE FINALLY!!!!!

---
+++ PhotonKrabs/Assets/DemoVikings/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	[HideInInspector]
+	public Transform[] spawnPoints;
+	
+	
+	// Use this for initialization
+	void Start () {
+		GameObject[] spawns = GameObject.FindGameObjectsWithTag("Respawn");
+		spawnPoints = new Transform[spawns.Length];
+		for(int i = 0; i < spawns.Length; i++) {
+			spawnPoints[i] = spawns[i].transform;
+		}
+	}
+
+
+	public Vector3 getSpawn() {
+		return spawnPoints[Random.Range (0, spawnPoints.Length)].position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
